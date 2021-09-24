e08f9e2313e72065b1f11dbf4fa01bd7594d89cf
Andy Park <apark1214@gmail.com>
2021-09-23 20:13:46 -0700

    making fetchPost accessable in the Component did mount method

--- my-app/src/components/postList.js
+++ my-app/src/components/postList.js
@@ -3,6 +3,11 @@ import {connect} from 'react-redux';
 import { fetchPosts } from '../actions';
 
 export class PostList extends Component{
+
+  componentDidMount(){
+    this.props.fetchPosts();
+  }
+
   render(){
     return (
       <div>Post List</div>
