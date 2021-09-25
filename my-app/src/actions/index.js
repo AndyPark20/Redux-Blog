3b0765fac28448150d8d43dcbb53c9bf7576a61e
Andy Park <apark1214@gmail.com>
2021-09-25 11:31:36 -0700

    adding action creator

--- my-app/src/actions/index.js
+++ my-app/src/actions/index.js
@@ -1,24 +1,38 @@
 import jsonPlaceHolder from "../apis/jsonPlaceHolder";
 
- export const fetchPosts =async() =>{
+export const fetchPosts = async () => {
 
-  // Bad approach! --> Breaking ruls of action creator!!!
-  const response = await jsonPlaceHolder.get('/posts');
-  //We think we are returning plain Js, but in reality because of Async await function,
-  //When FetchPost action creator is called, it is returning request function (jsonPlaceHolder)  rather than plain JS object.
-  //That is why we are seeing an error message!
+  return function (dispatch, getState) {
 
-  /***********************  OR *********************************/
-  //we can use WITHOUT AWAIT!
-  //However, the components can run faster than fetching data.
-  //As a result, by the time the reducer looks at the action form, there are no data because its still fetching!
+    // Bad approach! --> Breaking ruls of action creator!!!
+    const response =  jsonPlaceHolder.get('/posts');
+    //We think we are returning plain Js, but in reality because of Async await function,
+    //When FetchPost action creator is called, it is returning request function (jsonPlaceHolder)  rather than plain JS object.
+    //That is why we are seeing an error message!
 
-  //MIDDLEWARE
-  // Plain javascript function that gets called everytime it get dispatched.
-  //Has the ability to control, modify actions.
+    /***********************  OR *********************************/
+    //we can use WITHOUT AWAIT!
+    //However, the components can run faster than fetching data.
+    //As a result, by the time the reducer looks at the action form, there are no data because its still fetching!
 
-   return {
-     type:'FETCH_POSTS',
-     payload:response
-   };
- };
+    //MIDDLEWARE
+    // Plain javascript function that gets called everytime it get dispatched.
+    //Has the ability to control, modify actions.
+
+    //Redux thunk can return action objects, or it can return functions
+
+    return {
+      type: 'FETCH_POSTS',
+      payload: response
+    };
+  };
+};
+
+
+
+//You can also return action creators that are returning normal objectss!!!
+export const selectPost =()=>{
+  return{
+    type:'SELECT_POST'
+  }
+}
